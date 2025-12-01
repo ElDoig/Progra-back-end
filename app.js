3c3b69212c6ad57c7136c3e0a770e8c9224978c6
ElDoig <carlosdoigdn@gmail.com>
2025-11-30 23:21:26 -0500

    VER

--- app.js
+++ app.js
@@ -13,12 +13,7 @@ const app = express();
 
 
 app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
-    allowedHeaders: ["Content-Type", "Authorization"], 
-    credentials: true 
-  })
+  cors()
 );
 
 app.use(bodyParser.json()); 
